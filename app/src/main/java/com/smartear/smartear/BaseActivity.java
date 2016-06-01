d7d98422ceb5aec2966e3532f729855fb55ca562
Greg Bates <G.bates1991@gmail.com>
2016-06-01 14:07:56 +0500

    didi fragment animations. show recognized texts

--- app/src/main/java/com/smartear/smartear/BaseActivity.java
+++ app/src/main/java/com/smartear/smartear/BaseActivity.java
@@ -39,12 +39,12 @@ public class BaseActivity extends AppCompatActivity {
         fragment.setArguments(arguments);
 
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        if (Build.VERSION.SDK_INT >= 21) {
-            fragment.setEnterTransition(TransitionInflater.from(this).inflateTransition(android.R.transition.slide_right));
-            fragment.setExitTransition(TransitionInflater.from(this).inflateTransition(android.R.transition.fade));
-        } else {
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            fragment.setEnterTransition(TransitionInflater.from(this).inflateTransition(android.R.transition.slide_right));
+//            fragment.setExitTransition(TransitionInflater.from(this).inflateTransition(android.R.transition.fade));
+//        } else {
             fragmentTransaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
-        }
+//        }
         if (addToBackStack) {
             fragmentTransaction.addToBackStack(fragment.getFragmentTag());
         }
